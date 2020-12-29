3b9c055e6691b2407ba0e7b6c0c5068ca42d8b31
selkasse <selkasse@gmu.edu>
2020-12-28 20:11:42 -0500

    install plugins and set up navbar and skeleton pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    "gatsby-transformer-remark",
+    {
+      resolve: `gatsby-source-filesystem`,
+
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
 }
---
+++ src/pages/projects.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function Projects() {
+  return (
+    <Layout>
+      <h1>Projects placeholder</h1>
+    </Layout>
+  )
+}

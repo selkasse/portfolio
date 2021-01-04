1bbc71c7e835324df98f3f333dbdc7148dd9023d
selkasse <selkasse@gmu.edu>
2021-01-03 22:03:04 -0500

    fix colors on home page. fix navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
-import "./layout.module.css"
+import styles from "./layout.module.css"
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
 
+// style={{
+//   textShadow: `none`,
+//   backgroundImage: `none`,
+// }}
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
@@ -15,16 +19,18 @@ export default function Layout({ children }) {
           marginBottom: `1.5rem`,
           display: `flex`,
           justifyContent: `space-between`,
+          alignItems: `flex-start`,
         }}
       >
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>Sharif Elkassed</h3>
+        <Link to="/">
+          <h3 style={{ display: `inline block` }}>Sharif Elkassed</h3>
         </Link>
         {/* <ul style={{ listStyle: `none`, float: `right` }}> */}
         <ul
           style={{
             listStyle: `none`,
             display: `flex`,
+            marginTop: `2.175rem`,
           }}
         >
           {/* <ListLink to="/">Home</ListLink> */}

311fc7a30b23447a2717fa185e4d62256497c1a2
selkasse <selkasse@gmu.edu>
2021-01-05 01:01:26 -0500

    change favicon. add title

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
       },
     },
     "gatsby-transformer-remark",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
-import styles from "./layout.module.css"
+import { Helmet } from "react-helmet"
+import "./layout.module.css"
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
 
-// style={{
-//   textShadow: `none`,
-//   backgroundImage: `none`,
-// }}
 export default function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Sharif Elkassed</title>
+      </Helmet>
       <header
         style={{
           marginBottom: `1.5rem`,
@@ -25,7 +26,6 @@ export default function Layout({ children }) {
         <Link to="/">
           <h3 style={{ display: `inline block` }}>Sharif Elkassed</h3>
         </Link>
-        {/* <ul style={{ listStyle: `none`, float: `right` }}> */}
         <ul
           style={{
             listStyle: `none`,
@@ -33,7 +33,6 @@ export default function Layout({ children }) {
             marginTop: `2.175rem`,
           }}
         >
-          {/* <ListLink to="/">Home</ListLink> */}
           <ListLink to="/projects/">Projects</ListLink>
           <ListLink to="/blog/">Blog</ListLink>
           <ListLink to="/contact/">Contact</ListLink>
